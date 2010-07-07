eb41bedf8fde3626161f4073b1782a6479621e95
Ondrej Certik <ondrej@certik.cz>
2010-07-07 15:13:46 -0700

    t

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+#! /usr/bin/env python
+
 import cppwrap
 
 a = cppwrap.DoubleKeeper(5)
+b = cppwrap.DoubleKeeper2(5)
+print a.transmogrify(6)
+print b.transmogrify(6)
 print cppwrap.transmogrify_from_cpp(a, 6)
+print cppwrap.transmogrify_from_cpp(b, 6)
